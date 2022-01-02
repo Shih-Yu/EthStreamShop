8e1ad5be71f34c1919abd9303ca0a0532f6f81d0
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-01-02 12:10:11 -0600

    complete buy merch function, add getMerch function

--- contracts/EthStreamShop.sol
+++ contracts/EthStreamShop.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.6;
     -Use Chainlink to display current price of Eth/USD
     -Allow seller to input price
     -Tranfers money from buyer to seller
-    -Contract gets 10% commission for items sold
+    -Contract gets 20% commission for items sold
     -Struct of item information
     -Time limit for item listed (block.timestamp)
 */
@@ -17,13 +17,13 @@ contract EthStreamShop {
   address payable public seller;
   address payable public buyer;
 
-  // Merchandise number for each item to track
+// Merchandise number for each item to track
   uint public merchandiseNumber;
 
-  //Assigns a number to each merchandise added 
+//Assigns a number to each merchandise added 
   mapping(uint => Merchandise) public merchandise;
 
-
+// Information on each merchandise for sell
   struct Merchandise {
     string name;
     uint merchNum;
@@ -47,10 +47,25 @@ contract EthStreamShop {
   }
 
 
-  /* Function to add merch:
+  /* Function to add merch for sale with following information:
+      -Name
+      -Merch number
+      -Amount for sale
+      -Price -> convert to Eth
+      -Availablility
+      -Time limit
+      -Commission fee
+      -Buyer address
+      -Seller address
+      -Owner address
+    *Only owner of contract can add merchandise
+    *Increment merchandise number by one
+    *Confirm merchandise is added
   */
 
   function addMerch(address _seller, string memory _merchName, uint _price, uint _amount, uint _timeLeft) public returns(bool) {
+
+    require(msg.sender == owner, "Must be owner to add merchandise");
 
      merchandise[merchandiseNumber] = Merchandise({
       name: _merchName,
@@ -71,27 +86,39 @@ contract EthStreamShop {
 
   /* Function to buy merch:
     -Only available if time is not expired
+    -Must have enough merchandise to sell
     -Owner gets 20% of merch price sold
     -Seller gets price - commission
     -Buyer must have sufficient funds
-    -Merchandise is now sold
+    - Mark merchandise to sold
+    -Subtract amount bought from supply
+    -Owner of contract cannot buy merchandise
   */
-  function buyMerch(uint _merchandiseNumber) payable public {
+  function buyMerch(uint _merchandiseNumber, uint _amount) payable public {
     require(merchandise[_merchandiseNumber].isSold == false, "Merchandise is not available");
-    require(merchandise[_merchandiseNumber].timeLeft <= block.timestamp);
+    require(merchandise[_merchandiseNumber].timeLeft <= block.timestamp, "Time is over for this merchandise");
+    require(merchandise[_merchandiseNumber].amountToSell > 0, "Out of merchandise");
+    require(msg.sender != owner);
 
     merchandise[_merchandiseNumber].seller.transfer(merchandise[_merchandiseNumber].price - merchandise[_merchandiseNumber].commissionFee);
     merchandise[_merchandiseNumber].buyer = payable(msg.sender);
     merchandise[_merchandiseNumber].owner.transfer(merchandise[_merchandiseNumber].commissionFee);
+    merchandise[_merchandiseNumber].amountToSell - _amount;
     merchandise[_merchandiseNumber].isSold = true;
-    
-     
   }
 
-  // Function to sell merch
-  function sellMerch() payable public {
+// Function to get merchandise
+  function getMerch(uint _merchNumber) public view returns(string memory name, uint merchNum, uint amountToSell, uint price, bool isSold, uint timeLeft, address _seller) {
+   name =  merchandise[_merchNumber].name;
+   merchNum = merchandise[_merchNumber].merchNum;
+   amountToSell = merchandise[_merchNumber].amountToSell;
+   price = merchandise[_merchNumber].price;
+   isSold = merchandise[_merchNumber].isSold;
+   timeLeft = merchandise[_merchNumber].timeLeft;
+   _seller = merchandise[_merchNumber].seller;
 
+   return(name, merchNum, amountToSell, price, isSold, timeLeft, _seller);
+  
   }
-
 }
 
